cb46c8c4090a8105fe1c22066c0d5ddb95bad5b8
Miki Takahashi <math.micky.dtl@gmail.com>
2019-04-06 14:59:00 +0900

    added minus button

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -1,20 +1,23 @@
-//
-//  ViewController.swift
-//  Count
-//
-//  Created by Miki Takahashi on 2019/03/28.
-//  Copyright © 2019 MikiTakahashi. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "\(count)"
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        count += 1
+        label.text = "\(count)"
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        count -= 1
+        label.text = "\(count)"
+    }
+    
 }
 
